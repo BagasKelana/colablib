2a1cc8b53391940272aa09d760ba270bb4665e38
BagasKelana <141896859+BagasKelana@users.noreply.github.com>
2023-08-25 03:32:58 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colablib',
-    version='0.1.8',
+    version='0.1.7',
     packages=find_packages(),
     install_requires=[
         'safetensors==0.3.3',
